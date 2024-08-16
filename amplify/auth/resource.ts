253670530fb81afe62ce7e642b70d83b6a15d4a2
Hugo de Groot <hugodegroot@coffeeit.nl>
2024-08-16 13:49:42 +0200

    Set multifactor to sms instead of totp

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,10 +7,11 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    phone: true,
   },
 
   multifactor: {
     mode: 'REQUIRED',
-    totp: true,
-  }
+    sms: true,
+  },
 });
